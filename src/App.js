a2d3f78c30aa0cc8c05b7e95e3e9b6c3461c3c01
runeleaf <runeleaf@gmail.com>
2022-12-19 16:42:05 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./styles.css";
+import Board from "@asseinfo/react-kanban";
+import "@asseinfo/react-kanban/dist/styles.css";
+
+export default function App() {
+  const board = {
+    columns: [
+      {
+        id: 1,
+        title: "todo",
+        cards: [{ id: 1, title: "テスト", description: "内容" }]
+      },
+      { id: 2, title: "in progress", cards: [] },
+      { id: 3, title: "done", cards: [] }
+    ]
+  };
+
+  const onNewCard = (draftCard) => ({
+    id: new Date().getTime(),
+    ...draftCard
+  });
+
+  const onNewColumn = (draftColumn) => ({
+    id: new Date().getTime(),
+    ...draftColumn
+  });
+
+  return (
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <Board
+        initialBoard={board}
+        allowAddCard={{ on: "top" }}
+        allowAddColumn={{ on: "right" }}
+        allowRemoveCard
+        allowRenameColumn
+        onCardNew={console.log}
+        onCardRemove={console.log}
+        onColumnNew={console.log}
+        onColumnRemove={console.log}
+        onColumnRename={console.log}
+        onNewCardConfirm={onNewCard}
+        onNewColumnConfirm={onNewColumn}
+      />
+    </div>
+  );
+}
